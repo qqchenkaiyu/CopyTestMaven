3fb7be25292def2c819a04e8821328770cfc1b31
qqchenkaiyu <593352046@qq.com>
2019-04-13 12:43:48 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,45 @@
+import Test.TestCopy;
+import pojo.Boy;
+import pojo.Girl;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Boy boy = new Boy();
+        boy.setName("davy");
+        boy.setWeight("80kg");
+        boy.setHeight("175");
+        boy.setAge(18);
+
+        Girl girl = new Girl();
+        girl.setName("lili");
+        girl.setWeight("40kg");
+        girl.setHeight("165");
+        girl.setAge(18);
+        boy.setGirl(girl);
+
+
+        TestCopy testCopy = new TestCopy();
+        int Times=10000*1000;
+        testCopy.testSetter(Times,boy);
+        testCopy.testFuzhi(Times,boy);
+        testCopy.testSpringCopy(Times,boy);
+
+
+
+        try {
+            testCopy.testMyReflect(Times,boy);
+            testCopy.testCGLIB(Times,boy);
+            testCopy.testSystemClone(Times,boy);
+            testCopy.testCommonCopy(Times,boy);
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
